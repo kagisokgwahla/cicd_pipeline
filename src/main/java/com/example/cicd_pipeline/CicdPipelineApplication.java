09a3ebb70ac34f63432d54934f24cfe4ab921552
kagiso <kagisokgwahla@icloud.com>
2022-09-12 00:28:02 +0200

    first commit

---
+++ src/main/java/com/example/cicd_pipeline/CicdPipelineApplication.java
@@ -0,0 +1,22 @@
+package com.example.cicd_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdPipelineApplication {
+
+	@GetMapping("/hello")
+	public String welcome(){
+		return "Hello World!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPipelineApplication.class, args);
+	}
+
+}
